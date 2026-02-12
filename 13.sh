157f71ecbe0630aa48b1c9224e2b16fdc3660a49
ASHFAKH M <ashfakhthedev@gmail.com>
2026-02-12 14:04:23 +0530

    modified 13 th program for simple understanding

--- 13.sh
+++ 13.sh
@@ -1,33 +1,19 @@
-#!/usr/bin/bash
-echo -n "Enter first number :"
-read n1
-echo -n "Enter the second number :"
-read n2
-echo -n "Enter the operation ( + , - , * , / ): "
-read op
+#!/usr/bin/env bash
 
-case "$op" in
-	+)
-		r=$((n1 + n2))
-		echo "Result: $r"
-		;;
-	-)
-		r=$((n1 - n2))
-		echo "Result: $r"
-		;;
-	\*)
-		r=$((n1 * n2))
-		echo "Result: $r"
-		;;
-	/)
-		if [ "$n2" -eq 0 ] 2>/dev/null; then
-			echo "Error: division by zero"
-			exit 1
-		fi
-		r=$(echo "scale=4; $n1 / $n2" | bc -l)
-		echo "Result: $r"
-		;;
-	*)
-		echo "Invalid operator"
-		;;
+read -p "Enter first number: " n1
+read -p "Enter second number: " n2
+read -p "Enter operation (+ - * /): " op
+
+case $op in
+    +) echo "Result = $((n1 + n2))" ;;
+    -) echo "Result = $((n1 - n2))" ;;
+    \*) echo "Result = $((n1 * n2))" ;;
+    /)
+        if [ $n2 -eq 0 ]; then
+            echo "Cannot divide by zero"
+        else
+            echo "Result = $(echo "$n1 / $n2" | bc -l)"
+        fi
+        ;;
+    *) echo "Invalid operator" ;;
 esac

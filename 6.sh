8ca90f49fc681dbb67049b6ce1b41a01fdce8baf
ASHFAKH M <ashfakhthedev@gmail.com>
2026-02-12 13:47:16 +0530

    updated 6th program to simpler

--- 6.sh
+++ 6.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-echo "Enter three numbers (space-separated):"
-read a b c
 
-sum=$(echo "$a + $b + $c" | bc -l)
-mean=$(echo "($a + $b + $c) / 3" | bc -l)
-sd=$(echo "scale=6; sqrt((($a - $mean)^2 + ($b - $mean)^2 + ($c - $mean)^2) / 3)" | bc -l)
+read -p "Enter three numbers: " a b c
+
+sum=$((a + b + c))
+mean=$(echo "$sum / 3" | bc -l)
+sd=$(echo "sqrt((($a-$mean)^2 + ($b-$mean)^2 + ($c-$mean)^2)/3)" | bc -l)
 
 echo "sum=$sum"
 echo "mean=$mean"

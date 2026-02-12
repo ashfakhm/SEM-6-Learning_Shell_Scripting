c82299c7235edfbaf47de213bb778721d9f02915
ASHFAKH M <ashfakhthedev@gmail.com>
2026-02-12 13:53:35 +0530

    modified 9th program for simple understanding

--- 9.sh
+++ 9.sh
@@ -1,14 +1,17 @@
-#!/usr/bin/bash
-echo -n "Enter a number : "
-read limit
+#!/usr/bin/env bash
+
+read -p "Enter a number: " limit
+
 a=0
 b=1
-echo "The fibonacci numbers are : "
+
+echo "Fibonacci numbers:"
 echo -n "$a "
+
 while [ $b -lt $limit ]
 do
-echo -n "$b "
-c=$((a+b))
-a=$b
-b=$c
+    echo -n "$b "
+    next=$(( a + b ))  # calculate next number
+    a=$b               # shift values
+    b=$next
 done

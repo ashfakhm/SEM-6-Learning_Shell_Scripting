6b635dafd94ba0cd451d7d6eabd4ca7255a607ff
ASHFAKH M <ashfakhthedev@gmail.com>
2026-02-12 13:59:23 +0530

    modfied 10th program

--- 10.sh
+++ 10.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/bash
-echo "Enter a number : "
-read n
+#!/usr/bin/env bash
+
+read -p "Enter number: " n
+
 fact=1
-for ((i=1;i<=n;i++))
+
+for ((i=1; i<=n; i++))
 do
-fact=$((fact * i))
+    fact=$((fact * i))
 done
-echo "The factorial of $n is : $fact"
+
+echo "Factorial = $fact"

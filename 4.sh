9ef61c93e8ee50df68c2d02cd25066f25c647834
ASHFAKH M <ashfakhthedev@gmail.com>
2026-02-12 16:09:12 +0530

    simplified 4 th program

--- 4.sh
+++ 4.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-read -p "Enter a number :" n
+read -p "Enter a number: " n
 
-if ! [ "$n" -eq "$n" ] 2>/dev/null; then
-	echo "Invalid input: please enter an integer"
-	exit 1
-fi
-
-if [ $((n % 2)) -eq 0 ]; then
-	echo "Number is Even"
+if (( n % 2 == 0 )); then
+    echo "Even"
 else
-	echo "Number is Odd"
+    echo "Odd"
 fi
+

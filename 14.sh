e1a3fd87af0aca6b107b2c853338331f45389a3c
ASHFAKH M <ashfakhthedev@gmail.com>
2026-02-12 17:16:04 +0530

    modified 14th program

--- 14.sh
+++ 14.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-hour=$(date +%H)
+read -p "Enter hour (24-hour format): " hour
 
 if (( hour < 12 )); then
     echo "Good Morning!"
-elif (( hour < 16 )); then
-    echo "Good Afternoon!"
 else
     echo "Good Evening!"
 fi
-
